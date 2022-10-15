927500e4fb45b70501c04d231e963889ad737119
Joslin Some <joslin-some@uiowa.edu>
2022-10-15 11:24:19 -0500

    init

--- client/src/Navbar.js
+++ client/src/Navbar.js
@@ -2,6 +2,7 @@ import React, {useState} from 'react';
 import {Link} from 'react-router-dom'
 import { Icon } from '@iconify/react';
 import "./Navbar.css"
+import logo from './logo.png';
 function Navbar(props) {
     const [click,setClick] = useState(false);
 
@@ -9,10 +10,8 @@ function Navbar(props) {
     return (
         <div className='navbar'>
             <div className='navbar-container container'>
-                <Link to='/' className='navbar-logo'>
-                    <Icon icon="wi:thermometer" width='70' />
-                    Thermally
-                </Link>
+
+                <img src={logo}/>
 
                 <ul className={click ? 'nav-menu active' : 'nav-menu' }>
                     <li>
