647912a3963f10fa89b86c7a2a09a5cbd960227c
Joslin Some <joslin-some@uiowa.edu>
2022-10-15 11:16:14 -0500

    sterting

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,10 @@
 import logo from './logo.svg'
 import React, {useEffect} from 'react';
 import './App.css';
-
 import Navbar from "./Navbar";
+import {BrowserRouter as Router,Routes,Route} from "react-router-dom";
+
+
 
 function App() {
 
@@ -11,7 +13,14 @@ function App() {
     }, [])
 
     return (
-        <Navbar/>
+        <Router>
+            <Navbar/>
+            <Routes>
+                <Route path='/' />
+               <Route path='graph'/>
+            </Routes>
+        </Router>
+
     );
 }
 
--- client/src/Navbar.js
+++ client/src/Navbar.js
@@ -8,9 +8,30 @@ function Navbar(props) {
 
     return (
         <div className='navbar'>
+            <div className='navbar-container container'>
+                <Link to='/' className='navbar-logo'>
+                    <Icon icon="wi:thermometer" width='70' />
+                    Thermally
+                </Link>
 
-
-
+                <ul className={click ? 'nav-menu active' : 'nav-menu' }>
+                    <li>
+                        <Link to='/' className='nav-links'>
+                            Home
+                        </Link>
+                    </li>
+                    <li>
+                        <Link to='/graph' className='nav-links'>
+                            Graphs
+                        </Link><
+                    /li>
+                    <li>
+                        <Link to='/graph' className='nav-links'>
+                            Graphs
+                        </Link><
+                    /li>
+                </ul>
+            </div>
         </div>
     );
 }
